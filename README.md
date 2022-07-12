d501293af917cb695b398b8d7083d1232789324a
Rune Warhuus <rune@dinkdonk.no>
2022-07-12 20:03:34 +0200

    Settings

--- README.md
+++ README.md
@@ -7,11 +7,19 @@ Based on realtime data from [Nordpool](https://www.nordpoolgroup.com/en/Market-d
 ## Requirements
 
 - Raspberry PI
-- [Nedis WIFIPO120xxx](https://nedis.no/no-no/product/sikkerhet-personvern/smart-home/strom/550710067/smartlife-smart-plug-wi-fi-ip44-efektmaler-3680-w-jordet-kontakt-type-f-cee-77-30-40-0c-android-ios-gra-hvit)
+- [Nedis Smart Power Plug WIFIPO120xxx](https://nedis.no/no-no/product/sikkerhet-personvern/smart-home/strom/550710067/smartlife-smart-plug-wi-fi-ip44-efektmaler-3680-w-jordet-kontakt-type-f-cee-77-30-40-0c-android-ios-gra-hvit)
 
-## Setup Nedis smart socket
+## Setup
 
-- [Follow this guide](https://github.com/jasonacox/tinytuya)
+1. To get the Nedis smart power plug device `IP`, `ID` and `local key` [Follow this guide](https://github.com/jasonacox/tinytuya)
+2. Fill in `device_ip`, `device_id` and `device_local_key` in `Settings.toml`
+
+## Troubleshooting
+
+### Power plug is linked to `Nedis SmartLife` app, and not `Tuya Smart` app
+
+1. Reset the power plug by holding the power button for 6 seconds until the LED starts blinking.
+2. Add the power plug in the `Tuya Smart` app.
 
 ## Links
 
--- src/wall_socket.rs
+++ src/wall_socket.rs
@@ -4,8 +4,20 @@ use std::net::IpAddr;
 use std::str::FromStr;
 use std::collections::HashMap;
 use std::time::SystemTime;
+use config::Config;
 
 pub fn toggle_switch(on: bool) -> Result<()> {
+    let settings = Config::builder()
+        // Add in `./Settings.toml`
+        .add_source(config::File::with_name("Settings"))
+        .add_source(config::Environment::with_prefix("APP"))
+        .build()
+        .unwrap();
+
+    let device_ip: String = settings.get::<String>("device_ip").expect("`device_ip` needs to be set in Settings.toml");
+    let device_id: String = settings.get::<String>("device_id").expect("`device_id` needs to be set in Settings.toml");
+    let device_local_key: String = settings.get::<String>("device_local_key").expect("`device_local_key` needs to be set in Settings.toml");
+
     match on {
         true => println!("Turning on wall socket"),
         false => println!("Turning off wall socket")
@@ -17,15 +29,15 @@ pub fn toggle_switch(on: bool) -> Result<()> {
     let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() as u32;
 
     let payload = Payload::Struct(PayloadStruct{
-        dev_id: "bf4c7082353ecd63c6gusv".to_string(),
-        gw_id: Some("bf4c7082353ecd63c6gusv".to_string()),
+        dev_id: device_id.clone(),
+        gw_id: Some(device_id),
         uid: None,
         t: Some(current_time),
         dp_id: None,
         dps: Some(dps),
     });
 
-    let tuya_device = TuyaDevice::create("ver3.3", Some("2a7988f7abfabb68"), IpAddr::from_str("10.0.105.94").unwrap())?;
+    let tuya_device = TuyaDevice::create("ver3.3", Some(&device_local_key), IpAddr::from_str(&device_ip).unwrap())?;
 
     tuya_device.set(payload, 0)?;
 
